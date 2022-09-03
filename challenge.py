5235609963e2e9aeda268414ec25de1b3275c408
goyer <g.imbert34@gmail.com>
2022-09-03 15:44:49 +0200

    challenge.pt pulint_score=8

--- challenge.py
+++ challenge.py
@@ -1,7 +1,11 @@
+"""Module to handle challenges."""
+
 import json
 
 
 class Challenge():
+    """Class representing a challenge."""
+
     def __init__(self, id, chal_folder) -> None:
         self.id = id
         self.chal_file = chal_folder+str(self.id)+".json"
@@ -15,21 +19,24 @@ class Challenge():
         self.difficulty = self._read_chal_json("difficulty")
 
     def _read_chal_json(self, key):
-        with open(self.chal_file) as file:
+        with open(self.chal_file, "r", encoding="utf8") as file:
             res = json.load(file)[key]
         return res
 
     @property
     def inputs(self):
+        """Generator for challenge inputs."""
         for inp in self._inputs:
             yield inp
 
     @property
     def outputs(self):
+        """Generator for challenge outputs."""
         for out in self._outputs:
             yield out
 
     @property
     def io(self):
+        """Generator for challenge inputs/outputs."""
         for inp, out in zip(self.inputs, self.outputs):
             yield inp, out
