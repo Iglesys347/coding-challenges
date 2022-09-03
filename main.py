2ad72563291b698a33af18ee0e9bf01bce5faef3
goyer <g.imbert34@gmail.com>
2022-09-03 12:26:53 +0200

    main.py pylint_score=10

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+"""Main script of Coding-Challenges.
+
+This scripts does the following 3 things:
+    - check that redis & docker are installed
+    - check that a redis server is running on the specified host/port
+    - run the discord bot
+"""
+
+from subprocess import DEVNULL, STDOUT, CalledProcessError, check_call
+
 from bot import run_bot
 from db_utils import check_db_running
 from errors import DockerError, RedisError
-from subprocess import DEVNULL, STDOUT, CalledProcessError, check_call
 
 # checking that docker and redis are installed
 try:
     check_call(["dpkg", "-s", "docker-ce"], stdout=DEVNULL, stderr=STDOUT)
-except CalledProcessError:
-    raise DockerError("Docker is not installed on this machine")
+except CalledProcessError as exc:
+    raise DockerError("Docker is not installed on this machine") from exc
 try:
     check_call(["dpkg", "-s", "redis"], stdout=DEVNULL, stderr=STDOUT)
-except CalledProcessError:
-    raise RedisError("Redis is not installed on this machine")
+except CalledProcessError as exc:
+    raise RedisError("Redis is not installed on this machine") from exc
 
 # checking that redis db is running
 if not check_db_running():
