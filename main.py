178fb99f4e5a8a8e30c6cbd8e1784b618bc2d5d9
goyer <g.imbert34@gmail.com>
2022-09-04 16:23:06 +0200

    Add doc settigs.py

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ This scripts does the following 3 things:
 """
 
 from subprocess import DEVNULL, STDOUT, CalledProcessError, check_call
+import redis
 
 from bot import run_bot
 from db_utils import check_db_running
 from errors import DockerError, RedisError
+
+from settings import REDIS_HOST, REDIS_PORT, REDIS_DB
 
 # checking that docker and redis are installed
 try:
@@ -22,8 +25,11 @@ try:
 except CalledProcessError as exc:
     raise RedisError("Redis is not installed on this machine") from exc
 
+client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,
+                     db=REDIS_DB, decode_responses=True)
+
 # checking that redis db is running
-if not check_db_running():
+if not check_db_running(client):
     raise RedisError("Redis server is not running on the specified host/port")
 
 # running the bot
