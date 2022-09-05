d1ec4df818ecbf04119332c0cb4cb25368a3276c
Gauthier Imbert <g.imbert34@gmail.com>
2022-09-05 12:12:36 +0200

    Add test challenge.py

--- tests/test_challenge.py
+++ tests/test_challenge.py
@@ -6,13 +6,38 @@ from codchal.challenge import Challenge
 
 TEST_CHAL_ID = 1
 
+
 @pytest.fixture
 def challenge():
     return Challenge(TEST_CHAL_ID, CHALLENGES_DIR)
 
-def test_challenge_arguments_id(challenge):
-    assert challenge.id == TEST_CHAL_ID
 
-def test_challenge_arguments_chal_file(challenge):
-    assert challenge.chal_file == CHALLENGES_DIR+str(TEST_CHAL_ID)+".json"
-    
+@pytest.mark.parametrize("value,expected",
+                         [("name", "List sort"),
+                          ("desc", "Simply sort a list of integer in ascending order."),
+                          ("ex_inputs", ["1 2 3 4", "4 3 2 1"]),
+                          ("ex_outputs", ["1 2 3 4", "1 2 3 4"]),
+                          ("inputs", ["1 2 3 4", "4 3 2 1"]),
+                          ("outputs", ["1 2 3 4", "1 2 3 4"])])
+def test_challenge_read_chal_json(challenge, value, expected):
+    assert challenge._read_chal_json(value) == expected
+
+
+def generator_tester(generator, expected_vals):
+    for i, (actual, expected) in enumerate(zip(generator, expected_vals)):
+        assert i+1 <= len(expected_vals), 'Too many values returned from range'
+        assert expected == actual
+    assert i+1 == len(expected_vals), 'Too few values returned from range'
+
+
+def test_challenge_inputs(challenge):
+    generator_tester(challenge.inputs, ["1 2 3 4", "4 3 2 1"])
+
+
+def test_challenge_outputs(challenge):
+    generator_tester(challenge.outputs, ["1 2 3 4", "1 2 3 4"])
+
+
+def test_challenge_io(challenge):
+    generator_tester(
+        challenge.io, [("1 2 3 4", "1 2 3 4"), ("4 3 2 1", "1 2 3 4")])
