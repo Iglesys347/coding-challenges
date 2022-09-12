06853bc0627c5e2b3093f610261dbde91aee7dec
Gauthier Imbert <g.imbert34@gmail.com>
2022-09-12 09:39:00 +0200

    Fix pytest workflow

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -4,18 +4,19 @@ from redis.exceptions import ConnectionError
 
 from codchal import db_utils as du
 
-@pytest.mark.parametrize
+
+@pytest.fixture
 def redis_client():
     # using default settings
     client = redis.Redis()
     return client
 
+
 def test_db_utils_check_db_running(redis_client):
     assert du.check_db_running(redis_client) == True
+
 
 def test_db_utils_check_db_not_running():
     # defining a not running client
     client = redis.Redis("localhost", 6969)
-    with pytest.raises(ConnectionError):
-        du.check_db_running(client)
-
+    assert du.check_db_running(client) == False
